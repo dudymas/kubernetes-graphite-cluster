49c6b17d711429fe4def7d6cfa9a8de8b32e743b
Jeremy White <dudymas@gmail.com>
2019-10-09 01:32:58 -0400

    fix(graphite): correct migrate env vars

--- docker/graphite-master/entrypoint.sh
+++ docker/graphite-master/entrypoint.sh
@@ -6,6 +6,6 @@ mkdir -p /opt/graphite/storage/log/webapp
 touch /opt/graphite/storage/log/webapp/info.log
 chmod 0775 -R /opt/graphite/storage/log/webapp
 
-cd /opt/graphite/webapp/ && ./manage migrate --run-syncdb --noinput
+cd /opt/graphite/webapp/ && django-admin migrate --run-syncdb --noinput
 mkdir -p /var/log/supervisor
 exec /usr/bin/supervisord -c /etc/supervisord.conf
